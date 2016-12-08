18c8c06a38834086f25865c2af56d73973c4b740
guykap <actkapul@gmail.com>
2016-12-08 11:54:26 -0500

    files

---
+++ src/Job.java
@@ -0,0 +1,294 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+
+public class Job {
+
+	static String filename = "C:\\temp\\code\\castingNetworks\\test_file1.txt";
+	static int dailyMinPay = 30;
+	
+	//PRIVATE:
+	String notice;
+	String noticeLowerCase;
+	String currentOffer;
+
+    String offerRole;
+    String offerProjectName;
+    String offerTypeProject;
+    String offerRate;
+    String offerPaying;
+    String offerUnionStatus;
+    String offerPostedDate;
+    String offerListing;
+    String offerListingFirst;
+    String offerListingSex = "";
+	String offerListingEthnicity ="";
+	String offerListingNotes="";
+	String offerListingAgesHint="";
+	
+	boolean isSag;
+	boolean isEthnicity;
+	boolean isAge;
+	boolean isMale;
+	boolean isPayingEnough;
+	boolean isMaleName;
+	boolean isCar;
+	boolean isStandIn;
+	boolean reqSizes;
+	int grade;
+	String message;
+	boolean decisionSubmit;
+
+	public Job() {
+		message = new String("I would like to be considered for this production.  \n Thank you, \n Guy Kapulnik");
+		// this age temp for this test version 
+		isAge = true;
+	}
+	
+	public Job(String newNotice){
+		notice = new String (newNotice);
+		message = new String("I would like to be considered for this production.  \n Thank you, \n Guy Kapulnik");
+		// this age temp for this test version 
+		isAge = true;
+	}
+	
+
+      
+	public String getOfferRole(){return offerRole;};
+	public void setOfferRole(String newData){offerRole =newData; };
+ 
+	public String getOfferProjectName(){return offerProjectName;};
+	public void setOfferProjectName(String newData){offerProjectName =newData; };	
+	
+	public String getOfferTypeProject(){return offerTypeProject;};
+	public void setOfferTypeProject(String newData){offerTypeProject =newData; };	
+	
+	public String getOffertRate(){return offerRate;};
+	public void setOffertRate(String newData){offerRate =newData; };	
+	
+	public String getOfferPaying(){return offerPaying;};
+	public void setOfferPaying(String newData){offerPaying =newData; };
+	
+	public String getOfferUnionStatus(){return offerUnionStatus;};
+	public void setOfferUnionStatus(String newData){offerUnionStatus =newData; };
+	
+	public String getOfferPostedDate(){return offerPostedDate;};
+	public void setOfferPostedDate(String newData){offerPostedDate =newData; };
+	
+	public String getOfferListing(){return offerListing;};
+	public void setOfferListing(String newData){
+		offerListing =newData;
+		//format separate the 4 parts of the listing into Listing 1 ,2 ,3
+		//Principal / Female / All Ethnicities /19 - 65
+		//MUST BE DRESS SIZE 12-14. Real looking!
+		 
+		String delims = "[/,\n]";
+		String[] tokens= newData.split(delims);			
+			offerListingFirst = tokens[0];
+			offerListingSex =  tokens[1];
+			offerListingEthnicity =  tokens[2];			
+			offerListingAgesHint=tokens[3];
+			for(int i=4; i < tokens.length;i++)
+			{
+				offerListingNotes += tokens[i];
+			}
+	};
+	
+	public String getNotice(){return notice;};
+	public void setNotice(String newNotice){notice =newNotice; };
+	
+	public boolean getIsSag(){return isSag;};
+	public void setIsSag(boolean newBit){isSag = newBit;};
+	
+	public boolean getIsEthnicity(){return isEthnicity;};
+	public void setIsEthnicity(boolean newBit){isEthnicity = newBit;};
+	
+	public boolean getIsAge(){return isAge;};
+	public void setIsAge(boolean newBit){isAge = newBit;};
+	
+	public boolean getIsMale(){return isMale;};
+	public void setIsMale(boolean newBit){isMale = newBit;};
+	
+	public boolean getIsCar(){return isCar;};
+	public void setIsCar(boolean newBit){isCar = newBit;};
+	
+	public boolean getReqSizes(){return reqSizes;};
+	public void setReqSizes(boolean newBit){reqSizes = newBit;};
+	
+	public int getGrade(){return grade;};
+	public void setGrade(int newBit){grade = newBit;};
+	
+	public String getMessage(){return message;};
+	public void setMessage(String newMessage){message =newMessage; };
+	public void addToMessage(String newMessage){message += newMessage; };
+	
+	public boolean getDecisionSubmit(){return decisionSubmit;};
+	public void setDecisionSubmit(boolean newBit){decisionSubmit = newBit;};
+	
+	public void readNotice(){
+		//this reads the notice and sets all the Job params accordingly.
+
+		//SAG 
+		String noticeLowerCase = new String((this.notice).toLowerCase());	
+		if((noticeLowerCase.contains("\tsag"))
+				||(noticeLowerCase.contains(" sag"))
+				||(noticeLowerCase.startsWith("sag"))
+				||(noticeLowerCase.contains("\tunion"))
+				||(noticeLowerCase.contains(" union"))
+				||(noticeLowerCase.startsWith("union")))
+		{
+			setIsSag(true);			
+		}
+		
+		//MALE
+		if((noticeLowerCase.contains(" male"))
+		||(noticeLowerCase.startsWith("male"))
+		||(noticeLowerCase.contains(" men"))
+		||(noticeLowerCase.contains(" man "))
+		||(noticeLowerCase.contains("actor "))
+		||(noticeLowerCase.startsWith("men"))
+			||(offerListingSex.toLowerCase().contains(" male")))
+		{
+			setIsMale(true);
+		}
+				
+		
+		//There is a male name here for the character
+		
+		//this is a paying job
+		if (offerPaying.toLowerCase().contains("yes"))
+		{
+			isPayingEnough = true;
+			
+			// check the daily rate and make sure it is above dailyMinPay
+			// find the rate by analyzing String offerRate
+		}
+		//setIsMaleName(true);
+				 
+		//ETHNICITY
+		if((noticeLowerCase.contains("all ethnicities"))
+				||(noticeLowerCase.contains("caucasian"))
+				||(noticeLowerCase.startsWith("caucasian")))
+				{
+					setIsEthnicity(true);
+				}
+		
+		//CAR   
+		
+		if((noticeLowerCase.contains(" car "))
+				||(noticeLowerCase.startsWith("car "))
+				||(noticeLowerCase.contains("w/cars"))
+				||(noticeLowerCase.contains("mercedes"))
+				||(noticeLowerCase.contains("vehicle"))
+				||(noticeLowerCase.contains("bmw"))
+				||(noticeLowerCase.startsWith("car "))
+				||(noticeLowerCase.contains("cars")))
+				{
+					setIsCar(true);
+				}
+		
+		// read the AGE
+		
+		// option one:   20 - 30
+		// option two:   20s to 30s
+		// option three: early 30s
+		//option four: 30 something
+		
+		
+	}
+	
+	public void makeDecision(){
+		//if ((isSag)&&(isAge)&&(isMale)&&(!isCar)&&(isPayingEnough)){
+		if ((isAge)&&(isMale)&&(!isCar)&&(isPayingEnough)){
+			this.setDecisionSubmit(true);
+		}
+		
+		//DO NOT SUBMIT
+		
+	}
+	
+
+	public void fillTalentNote(){
+				//last time worked
+		if((noticeLowerCase.contains(" note last "))
+				||(noticeLowerCase.contains("please note if you have worked"))
+				||(noticeLowerCase.contains("worked on the"))
+				||(noticeLowerCase.contains("must not have worked on this project"))
+				||(noticeLowerCase.contains("last time that you worked"))
+				||(noticeLowerCase.contains("do not submit if you have worked on this show")))
+		{
+					this.addToMessage("I've never worked on the production.");
+		}
+		 
+		if((noticeLowerCase.contains("sizes"))
+				||(noticeLowerCase.contains("note all sizes"))
+				||(noticeLowerCase.contains("note neck")))
+		{
+					this.setMessage("height: 6'2\n weight:200\njacket:42\nneckXsleeve:16.5x35\nwaistXinseam:34x33\nshoe:11");
+		}
+		
+		if((noticeLowerCase.contains(" Please note if you can provide"))
+				||(noticeLowerCase.contains("own the wardrobe")))
+ 		{
+					this.addToMessage("I own the wardrobe.");
+		}
+		
+		this.improveMessage();
+	}
+
+	public void improveMessage(){
+		//checks the length and if empty - then add the basic message
+		if (getMessage().length()<5){
+			setMessage("I would like to be considered for this production.\nThank you,\nGuy Kapulnik")
+		}
+		//add the Thanks! Guy ending
+		if(!(getMessage().contains("Guy"))){
+			addToMessage("Thanks,\nGuy");
+		}
+	}
+	
+	public void loadNoticesFromFile(){
+		//read file
+		
+		  List<String> records = new ArrayList<String>();
+		  try
+		  {
+		    BufferedReader reader = new BufferedReader(new FileReader(filename));
+		    String line;
+		    while ((line = reader.readLine()) != null)
+		    {
+		      records.add(line);
+		    }
+		    reader.close();
+		    return;
+		  }
+		  catch (Exception e)
+		  {
+		    System.err.format("Exception occurred trying to read '%s'.", filename);
+		    e.printStackTrace();
+		    return;
+		  }
+	}
+	
+	 public static int randInt(int min, int max) {
+
+		    // Usually this can be a field rather than a method variable
+		    Random rand = new Random();
+		    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		    return randomNum;
+		}
+	public boolean sameJob(Job otherJob){
+		//returns true if the two Jobs have the same first 15 chars notice.
+		if(	(this.notice).contains(otherJob.getNotice().subSequence(1, 100))){
+			return true; 
+		}
+			return false;
+	}
+}
+
---
+++ src/Pamela.java
@@ -0,0 +1,165 @@
+
+
+//import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+//import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.util.concurrent.TimeUnit;
+
+public class Pamela {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();		
+  static private List<Job> Jobs = new ArrayList<Job>();
+  static Iterator<Job> jobIterator = Jobs.iterator();
+	
+
+  @Before
+  public void setUp() throws Exception {
+	  System.setProperty("webdriver.gecko.driver",
+			  "C:\\Users\\me\\work\\fifth\\selenium\\libs\\geckodriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "http://home.castingnetworks.com//";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testPamela() throws Exception {
+	System.out.println(" start test Pamela");	  
+    driver.get(baseUrl + "/");
+    TimeUnit.SECONDS.sleep(4);
+    driver.findElement(By.id("login")).click();
+    driver.findElement(By.id("login")).clear();
+    driver.findElement(By.id("login")).sendKeys("guykapulnik");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("cGuy1234567");
+    driver.findElement(By.xpath("//input[@id='submit']")).click();
+    TimeUnit.SECONDS.sleep(4);
+    driver.findElement(By.id("_ctl0_cphBody_rptProfiles__ctl1_lnkViewProfile2")).click();
+    System.out.println("Succ logging in");
+    driver.findElement(By.id("_ctl0_lnkDirectCast")).click();
+    driver.findElement(By.cssSelector("td > table > tbody > tr > td > a")).click();
+    new Select(driver.findElement(By.name("viewfilter"))).selectByVisibleText("All Roles");
+   // driver.findElement(By.id("_ctl0_lnkExtrasRoles")).click();
+    
+    //check that the first three options don't say 'remove' link on the left. 
+    //if it has remove - log it in the DB as 'submitted' and end 
+    //enter the offer into the db, and make decision.
+    //if decision is SUBMIT perform:
+    
+
+    String currentOffer = new String (driver.findElement(By.xpath("//tr[3]/td/a")).getText());
+    String currentOfferRole =  new String (driver.findElement(By.xpath("//tr[3]/td/a")).getText());
+    String currentOfferProjectName =  new String (driver.findElement(By.xpath("//tr[3]/td[2]/a")).getText());
+    String currentOfferTypeProject =  new String (driver.findElement(By.xpath("//tr[3]/td[3]/a")).getText());
+    String currentOffertRate =  new String (driver.findElement(By.xpath("//tr[3]/td[4]/a")).getText());
+    String currentOfferPaying =  new String (driver.findElement(By.xpath("//tr[3]/td[5]/a")).getText());
+    String currentOfferUnionStatus =  new String (driver.findElement(By.xpath("//tr[3]/td[6]/a")).getText());
+    String currentOfferPostedDate =  new String (driver.findElement(By.xpath("//tr[3]/td[7]/a")).getText());
+    String currentOfferListing =  new String (driver.findElement(By.xpath("//tr[4]/td")).getText());
+
+	
+    //enter into JOB class
+     
+    Job offer = new Job(currentOffer);
+    offer.setOfferRole(currentOffer);
+    offer.setOfferRole(currentOfferRole);
+    offer.setOfferProjectName(currentOfferProjectName);
+    offer.setOfferTypeProject(currentOfferTypeProject);
+    offer.setOffertRate(currentOffertRate);
+    offer.setOfferPaying(currentOfferPaying);
+    offer.setOfferUnionStatus(currentOfferUnionStatus);
+    offer.setOfferPostedDate(currentOfferPostedDate);
+    offer.setOfferListing(currentOfferListing);
+    System.out.println("Succ adding offer to Jobs list");
+    
+    //enter into MySQL
+    
+    offer.readNotice();
+    offer.makeDecision();
+    Jobs.add(offer);   
+    if(offer.getDecisionSubmit())
+    {
+    System.out.println("Begin submittion for this offer");	
+    	    //submitting to this offer
+    TimeUnit.SECONDS.sleep(randInt(1,2));
+    	
+    driver.findElement(By.xpath("//tr[3]/td/a")).click();
+    // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | ProjWin | 30000]]
+    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=ProjWin | ]]
+//    driver.findElement(By.linkText("submit")).click();
+  
+    driver.findElement(By.xpath("//table[6]/tbody/tr/td/table/tbody/tr/td")).click();
+    driver.findElement(By.id("TALENTNOTE")).clear();
+    driver.findElement(By.id("TALENTNOTE")).sendKeys(offer.getMessage());
+    driver.findElement(By.cssSelector("div > table > tbody > tr > td > a > img")).click();
+    driver.findElement(By.cssSelector("td.dotbottom > img")).click();
+    System.out.println("Succ submitting");
+    }else{
+    	//do not submit
+    }
+    System.out.println(" Decision : " + offer.getDecisionSubmit() + " SAG:" + offer.getIsSag() + " Male:"+ offer.getIsMale() + " Eth:" + offer.getIsEthnicity() + "Car: " + offer.isCar + " __ " + offer.getNotice());
+    
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  public static int randInt(int min, int max) {
+
+	    // Usually this can be a field rather than a method variable
+	    Random rand = new Random();
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+  
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
